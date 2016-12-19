77086b8fa3ca6e8aec4a80284284dc01fa9b3914
BlackStar <zirkaandry@gmail.com>
2016-12-19 10:26:57 +0200

    addNav

--- js/script_aency.js
+++ js/script_aency.js
@@ -1,162 +1,192 @@
 window.onload = function () {
 
+		console.log("Everything good");
 
-	console.log("Everything good");
+		var boxPhoto = $('.boxPhoto'),
+				leftGate = $('.leftGate'),
+				rightGate = $('.rightGate'),
 
+				offFilter = $('.offFilter'),
 
-		var boxPhoto= $('.boxPhoto'),
-			leftGate = $('.leftGate'),
-			rightGate = $('.rightGate'),
+				circleMin = $('.svg_circle_min'),
+				circle = $('.svg_circle'),
 
-			offFilter=$('.offFilter'),
+				rombMin = $('.svg_romb_min'),
+				rombMid = $('.svg_romb_mid'),
+				romb = $('.svg_romb'),
 
-			circleMin = $('.svg_circle_min'),
-			circle = $('.svg_circle'),
+				$navigationSite = $('.navigationSite'),
+				$navigationTop = $(".navigationTop"),
+				buttonNav = $('.my_menu_button'),
+				navBar = $(".navBar"),
 
-			rombMin =$('.svg_romb_min'),
-			rombMid =$('.svg_romb_mid'),
-			romb =$('.svg_romb'),
+				textForPhoto = $('.textForPhoto p');
 
-
-			navBar =$(".navBar"),
-
-
-			textForPhoto = $('.textForPhoto p');
-	
 //  Работа с навигацией сайта
 
-$(".navigationTop").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
-
-$('.my_menu_button').on("click",  function(){
-
-	// navBar.toggleClass('noneVis');
-
-	if ($(".navBar").is(":hidden")) {
-        navBar.slideDown();
-      } else {
-        navBar.slideUp();
-      }
-});
+	
 
 
 
+						// function changeNav() {
+						// 		var coord = $navigationTop.offset().top + $navigationTop.height();
+						// 		if ($navigationSite.hasClass("navMob") && $(window).scrollTop() < coord)
+						// 		 {
+						// 				buttonNav.css({display: "none"});
+						// 				navBar.show();
+						// 				$navigationSite.toggleClass("navMob")
+
+
+						// 		} else if ($(window).scrollTop() > coord) {
+						// 				buttonNav.css({display: "block"});
+						// 				navBar.hide();
+						// 				$navigationSite.toggleClass("navMob")
+						// 		}
+						// }
+
+						// $(window).scroll($.throttle(changeNav, 200))
+			
+
+var nav = $('.navigationSite'),
+    navPosTop =  $navigationTop.offset().top + $navigationTop.height(),
+    timer;
+$(window).on({
+    scroll: function() {
+        nav[$(this).scrollTop() > navPosTop ? 'addClass' : 'removeClass']('navMob');
+    },
+    resize: function() {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            console.log('action');
+            navPosTop = nav.offset().top;
+        }, 200);
+    }
+}).resize();
+
+
+
+
+
+// console.log($navigationTop.offset().top + " =top elementa");
+// console.log($navigationTop.offset().left + " =left elementa");
+
+// console.log($navigationTop.scrollTop() + " =Проскроллено топ elementa");
+
+		$navigationTop.on("click", "a", function (event) { // прокрутка страници
+				event.preventDefault();
+				var id = $(this).attr('href'),
+						top = $(id).offset().top;
+				$('body,html').animate({scrollTop: top}, 800);
+		});
+
+		$('.my_menu_button').on("click", function () { // Спрятать-показать навигацию сайта
+
+				navBar.toggleClass('noneVis');
+
+				if (navBar.is(":hidden")) {
+						navBar.slideDown();
+				} else {
+						navBar.slideUp();
+				}
+		});
 
 // carusel
 //Каруселька
-	//Документация: http://owlgraphic.com/owlcarousel/
+		//Документация: http://owlgraphic.com/owlcarousel/
 
-var owl = $('.owl-carousel');
-	owl.owlCarousel({
-		items : 1,
-		 loop:true,
-		 autoHeight: true
-		 
-	});
+		var owl = $('.owl-carousel');
+		owl.owlCarousel({
+				items: 1,
+				loop: true,
+				autoHeight: true
 
-	owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
+		});
 
-	$(".next_button").click(function(){
-		owl.trigger("next.owl");
-	});
-	$(".prev_button").click(function(){
-		owl.trigger("prev.owl");
-	});
+		owl.on('mousewheel', '.owl-stage', function (e) {
+				if (e.deltaY > 0) {
+						owl.trigger('next.owl');
+				} else {
+						owl.trigger('prev.owl');
+				}
+				e.preventDefault();
+		});
 
-
-
-
+		$(".next_button").click(function () {
+				owl.trigger("next.owl");
+		});
+		$(".prev_button").click(function () {
+				owl.trigger("prev.owl");
+		});
 
 // Box Model Работа с фильтрами и анимацией
 
-boxPhoto.hover(function(event){
+		boxPhoto.hover(function (event) {
 
-              $(this).find(leftGate).animate({left:'-40%'}, 1000); 
- 			  $(this).find(rightGate).animate({left:'100%'}, 1000); 
+				$(this).find(leftGate).animate({left: '-40%'}, 1000);
+				$(this).find(rightGate).animate({left: '100%'}, 1000);
 
- 			  $(this).find(romb).animate({opacity: 0}, 1000);
-			  $(this).find(rombMid).animate({opacity: 0}, 1000);
- 			  $(this).find(rombMin).animate({opacity: 0}, 1000);
+				$(this).find(romb).animate({opacity: 0}, 1000);
+				$(this).find(rombMid).animate({opacity: 0}, 1000);
+				$(this).find(rombMin).animate({opacity: 0}, 1000);
 
- 			  $(this).find(circle).animate({opacity: 0}, 700);
- 			  $(this).find(circleMin).animate({opacity: 0}, 700);
+				$(this).find(circle).animate({opacity: 0}, 700);
+				$(this).find(circleMin).animate({opacity: 0}, 700);
 
-  			  $(this).find('div[class^="ph_"]').toggleClass('offFilter'); // неясно насколько нужен эффект
- 			  $(this).find(textForPhoto).toggleClass('colorizeText');  
+				$(this).find('div[class^="ph_"]').toggleClass('offFilter'); // неясно насколько нужен эффект
+				$(this).find(textForPhoto).toggleClass('colorizeText');
 
-}, function(event){
+		}, function (event) {
 
+				$(this).find(leftGate).animate({left: '0%'}, 800)
+				$(this).find(rightGate).animate({left: '60%'}, 800);
 
-	  $(this).find(leftGate).animate({left:'0%'}, 800)
-	  $(this).find(rightGate).animate({left:'60%'}, 800);
-	  
-	  $(this).find(romb).animate({opacity: 1}, 1000);
-	  $(this).find(rombMid).animate({opacity: 1}, 1000);
- 	  $(this).find(rombMin).animate({opacity: 1}, 1000);
+				$(this).find(romb).animate({opacity: 1}, 1000);
+				$(this).find(rombMid).animate({opacity: 1}, 1000);
+				$(this).find(rombMin).animate({opacity: 1}, 1000);
 
- 	  $(this).find(circle).animate({opacity: 1}, 1000);
- 	  $(this).find(circleMin).animate({opacity: 1}, 1000);
+				$(this).find(circle).animate({opacity: 1}, 1000);
+				$(this).find(circleMin).animate({opacity: 1}, 1000);
 
- 	  $(this).find('div[class^="ph_"]').toggleClass('offFilter'); // неясно насколько нужен эффект
- 	  $(this).find(textForPhoto).toggleClass('colorizeText'); 
+				$(this).find('div[class^="ph_"]').toggleClass('offFilter'); // неясно насколько нужен эффект
+				$(this).find(textForPhoto).toggleClass('colorizeText');
 
-
-	}); 
-
-
-
+		});
 
 //  CaSTING DIV
 
-	
+		var $castingPage = $('.castingPage'),
+				$contactBut = $('.contactBut');
 
+		$contactBut.on("click", function () {
 
- $('.contactBut').on("click", function(){
+				$castingPage.css({display: 'block'});
+		})
 
+		$('.closeBut').on('click', function () {
+				$castingPage.css({display: 'none'});
+		})
 
- 	$('.castingPage').css({display: 'block'});
- })
+		var boxWidth = 250;
 
+		function centerBox() {
 
+				/* определяем нужные данные */
+				var winWidth = $(window).width();
+				var winHeight = $(document).height();
+				var scrollPos = $(window).scrollTop();
 
-    var boxWidth = 250;
+				/* Вычисляем позицию */
 
-    function centerBox() {
+				var disWidth = (winWidth - boxWidth) / 4;
+				var disHeight = scrollPos;
 
-        /* определяем нужные данные */
-        var winWidth = $(window).width();
-        var winHeight = $(document).height();
-        var scrollPos = $(window).scrollTop();
+				/* Добавляем стили к блокам */
+				$castingPage.css({'left': disWidth + 'px', 'top': disHeight + 'px'});
+				// $('.bigCasting').css({ 'width': winWidth + 'px', 'height': winHeight + 'px' });
 
-        /* Вычисляем позицию */
+				return false;
+		}
 
-        var disWidth = (winWidth - boxWidth) / 4 ;
-        var disHeight = scrollPos ;
-
-        /* Добавляем стили к блокам */
-        $('.castingPage').css({ 'left': disWidth + 'px', 'top': disHeight + 'px' });
-        // $('.bigCasting').css({ 'width': winWidth + 'px', 'height': winHeight + 'px' });
-
-        return false;
-    }
-    centerBox();
-
-
-
-
-
-
-
+		centerBox();
 
 }
